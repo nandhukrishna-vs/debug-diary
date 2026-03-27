bf614a27c64104e891a6f15b0a2f33c30758afc0
nandhukrishna <nandhukrishna296@gmail.com>
2026-03-27 10:05:05 +0530

    Time: 2 ms (7.94%), Space: 55.8 MB (5.39%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let len=0;
+    let x=s.trim();
+
+    for(let i=0;i<x.length;i++){
+        if(x[i]==" "){
+            len=0
+        }else{
+            len++;
+        }
+    }
+    return len;
+};
