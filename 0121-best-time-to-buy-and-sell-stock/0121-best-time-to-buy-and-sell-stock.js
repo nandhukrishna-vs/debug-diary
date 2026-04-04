809927583d40bec99cfb19eeb310e67f5d2888e1
nandhukrishna <nandhukrishna296@gmail.com>
2026-04-04 14:02:50 +0530

    Time: 1 ms (95.09%), Space: 65.1 MB (68.14%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minprice=prices[0];
+    let maxprofit=0;
+
+    for(let i=0;i<prices.length;i++){
+        if(prices[i]<minprice){
+            minprice=prices[i];
+        }else{
+            let profit=prices[i]-minprice
+            if(profit>maxprofit){
+                maxprofit=profit;
+            }
+        }
+    }
+    return maxprofit;
+};
