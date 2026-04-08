1f1858a5c99cae77295af0e9ca56dc329a290f73
nandhukrishna <nandhukrishna296@gmail.com>
2026-04-08 16:18:44 +0530

    Time: 0 ms (100%), Space: 53 MB (91.9%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var commonFactors = function(a, b) {
+   let count=0
+   let min= Math.min(a,b);
+   for(let i=1;i<=min;i++){
+    if(a%i==0 && b%i==0){
+        count++;
+    }
+   }
+   return count;
+};
