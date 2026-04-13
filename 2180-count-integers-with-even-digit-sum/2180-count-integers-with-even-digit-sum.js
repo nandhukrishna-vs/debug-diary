c1a7ac440c41dd391131e1a17e8baaf12f0385c0
nandhukrishna <nandhukrishna296@gmail.com>
2026-04-13 14:04:47 +0530

    Time: 1 ms (88.59%), Space: 55.8 MB (34.23%) - LeetHub

---
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var countEven = function(num) {
+    let count=0
+    for(let i=1;i<=num;i++){
+        let sum=0;
+        let n=i;
+
+        while(n>0){
+            sum+=n%10;
+            n=Math.floor(n/10);
+        }
+        if(sum%2==0){
+            count++;
+        }
+    }
+    return count;
+};
